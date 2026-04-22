3bede21809a412a0efb61567d9ff8cb0c5f46a67
Xavier Seignard <xavier.seignard@gmail.com>
2026-04-22 11:12:26 +0200

    chore: move example apps in examples folder (#41)

--- examples/example-new-arch/script/preTestFlight.ts
+++ examples/example-new-arch/script/preTestFlight.ts
@@ -4,7 +4,7 @@ import { join } from "node:path";
 const packagePath = join(__dirname, "../package.json");
 const libraryPackagePath = join(
   __dirname,
-  "../../packages/react-native-nitro-vto/package.json"
+  "../../../packages/react-native-nitro-vto/package.json"
 );
 
 const packageJson = JSON.parse(readFileSync(packagePath, "utf-8"));
@@ -19,5 +19,5 @@ packageJson.dependencies["@alaneu/react-native-nitro-vto"] = version;
 writeFileSync(packagePath, JSON.stringify(packageJson, null, 2) + "\n");
 
 console.log(
-  `Updated @alaneu/react-native-nitro-vto version to ${version} in example/package.json`
+  `Updated @alaneu/react-native-nitro-vto version to ${version} in examples/example-new-arch/package.json`
 );
